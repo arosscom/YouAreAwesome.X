8fd765eacd435fb0ad989ddebf794dbd3e2a851c
A. Ross Morton <arossco@ntlworld.com>
2025-10-12 14:24:18 +0100

    Initial Commit

---
+++ YouAreAwesome.X/YouAreAwesome_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome_XApp.swift
+//  YouAreAwesome.X
+//
+//  Created by Ross Morton on 12/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
